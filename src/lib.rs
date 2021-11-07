7eeaac3c3659f21c1921aa2fe7c73a232b2ee2fc
LouisaMedova <luiza.medova@phystech.edu>
2021-11-07 11:48:09 +0300

    added  instruction

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use solana_program::{
     entrypoint,
     entrypoint::ProgramResult,
     msg,
-    program::invoke,
+    program::{invoke, invoke_signed},
     program_error::ProgramError,
     pubkey::Pubkey,
     system_instruction,
@@ -13,11 +13,14 @@ use solana_program::{
 entrypoint!(process_instruction);
 
 const N: u64 = 100000000;
+pub const WALLET_A_SEED: &str = "wallet_a";
+pub const WALLET_B_SEED: &str = "wallet_b";
 
 #[derive(Debug, BorshSerialize, BorshDeserialize)]
 pub enum SwapInstruction {
     SwapAtoB { amount: u64 },
     SwapBtoA { amount: u64 },
+    CreateAccounts { amount_a: u64, amount_b: u64 }
 }
 
 /// Accounts expected:
@@ -25,24 +28,26 @@ pub enum SwapInstruction {
 /// 1. `[writable]` Credit lamports to this account
 /// 2. `[]` System program
 pub fn process_instruction(
-    _program_id: &Pubkey,
+    program_id: &Pubkey,
     accounts: &[AccountInfo],
     input: &[u8],
 ) -> ProgramResult {
     let acc_iter = &mut accounts.iter();
-    let user_wallet1 = next_account_info(acc_iter)?;
-    let user_wallet2 = next_account_info(acc_iter)?;
-    let wallet_a_info = next_account_info(acc_iter)?;
-    let wallet_b_info = next_account_info(acc_iter)?;
-
-    let balance_a = wallet_a_info.lamports();
-    let balance_b = wallet_b_info.lamports();
-
+    
     match SwapInstruction::try_from_slice(input)? {
         SwapInstruction::SwapAtoB { amount } => {
+            let user_wallet1 = next_account_info(acc_iter)?;
+            let user_wallet2 = next_account_info(acc_iter)?;
+            let wallet_a_info = next_account_info(acc_iter)?;
+            let wallet_b_info = next_account_info(acc_iter)?;
+
+            let balance_a = wallet_a_info.lamports();
+            let balance_b = wallet_b_info.lamports();
+
             if !user_wallet1.is_signer {
                     return Err(ProgramError::MissingRequiredSignature);
                 }
+
             let dy =  balance_b*N - (balance_a*balance_b*N)/(balance_a + amount);
 
             invoke(
@@ -56,6 +61,14 @@ pub fn process_instruction(
             msg!("Swap A to B  done");
         }
         SwapInstruction::SwapBtoA { amount } => {
+            let user_wallet1 = next_account_info(acc_iter)?;
+            let user_wallet2 = next_account_info(acc_iter)?;
+            let wallet_a_info = next_account_info(acc_iter)?;
+            let wallet_b_info = next_account_info(acc_iter)?;
+
+            let balance_a = wallet_a_info.lamports();
+            let balance_b = wallet_b_info.lamports();
+
             if !user_wallet2.is_signer {
                 return Err(ProgramError::MissingRequiredSignature);
             }
@@ -71,6 +84,53 @@ pub fn process_instruction(
             **user_wallet1.try_borrow_mut_lamports()? += dx/N;
 
             msg!("Swap B to A  done");
+        }
+        SwapInstruction::CreateAccounts{ amount_a, amount_b } => {
+            let payer = next_account_info(acc_iter)?;
+            let wallet_a_info = next_account_info(acc_iter)?;
+            let wallet_b_info = next_account_info(acc_iter)?;
+            let system_program_info = next_account_info(acc_iter)?;
+
+            if !payer.is_signer {
+                return Err(ProgramError::MissingRequiredSignature);
+            }
+
+            let (pda_a_pubkey, bump_seed_a) = Pubkey::find_program_address(&[WALLET_A_SEED.as_bytes()], program_id);
+            let (pda_b_pubkey, bump_seed_b) = Pubkey::find_program_address(&[b"wallet_b"], program_id);
+
+            if pda_a_pubkey != *wallet_a_info.key {
+                return Err(ProgramError::InvalidArgument);
+            }
+
+            if pda_b_pubkey != *wallet_b_info.key {
+                return Err(ProgramError::InvalidArgument);
+            }
+
+            let signer_seeds_a: &[&[_]] = &[WALLET_A_SEED.as_bytes(), &[bump_seed_a]];
+            invoke_signed(
+                &system_instruction::create_account(
+                    payer.key,
+                    &pda_a_pubkey,
+                    amount_a,
+                    0,
+                    program_id,
+                ),
+                &[payer.clone(), wallet_a_info.clone(), system_program_info.clone()],
+                &[&signer_seeds_a],
+            )?;
+
+            let signer_seeds_b: &[&[_]] = &[WALLET_B_SEED.as_bytes(), &[bump_seed_b]];
+            invoke_signed(
+                &system_instruction::create_account(
+                    payer.key,
+                    &pda_b_pubkey,
+                    amount_b,
+                    0,
+                    program_id,
+                ),
+                &[payer.clone(), wallet_b_info.clone(), system_program_info.clone()],
+                &[&signer_seeds_b],
+            )?;
         }
     }
 
--- tests/functional.rs
+++ tests/functional.rs
@@ -15,36 +15,17 @@ async fn test_swap() {
     let program_id = Pubkey::from_str(&"Swap111111111111111111111111111111111111111").unwrap();
 
     let (wallet_a_pubkey, _) =
-    Pubkey::find_program_address(&[b"walletA"], &program_id);
+    Pubkey::find_program_address(&[b"wallet_a"], &program_id);
 
     let (wallet_b_pubkey, _) =
-    Pubkey::find_program_address(&[b"walletB"], &program_id);
+    Pubkey::find_program_address(&[b"wallet_b"], &program_id);
 
-    let mut program_test = ProgramTest::new(
+    let program_test = ProgramTest::new(
         "swap", 
         program_id, 
         processor!(process_instruction),
     );
   
-
-    program_test.add_account(
-        wallet_a_pubkey,
-        Account {
-            lamports: 1000,
-            owner: program_id,
-            ..Account::default()
-        },
-    );
-
-    program_test.add_account(
-        wallet_b_pubkey,
-        Account {
-            lamports: 1000,
-            owner: program_id,
-            ..Account::default()
-        },
-    );
-
     let wallet1 = Keypair::new();
     let wallet2 = Keypair::new();
 
@@ -72,6 +53,32 @@ async fn test_swap() {
     .unwrap();
 
     let mut tx = Transaction::new_with_payer(
+        &[Instruction::new_with_borsh(
+            program_id,
+            &SwapInstruction::CreateAccounts { amount_a: 1000, amount_b: 1000 } ,
+            vec![
+                AccountMeta::new(wallet1.pubkey(), false),
+                AccountMeta::new(wallet_a_pubkey, false),
+                AccountMeta::new(wallet_b_pubkey, false),
+                AccountMeta::new(system_program::id(), false),
+            ],
+        )],
+        Some(&wallet1.pubkey()),
+    );
+
+    tx.sign(&[&wallet1], ctx.last_blockhash);
+    ctx.banks_client.process_transaction(tx).await.unwrap();
+
+    let wallet_a = ctx.banks_client
+        .get_account(wallet_a_pubkey).await.expect("get_account").expect("associated_account not none");
+    
+    let wallet_b = ctx.banks_client
+        .get_account(wallet_b_pubkey).await.expect("get_account").expect("associated_account not none");
+
+    assert_eq!(wallet_a.lamports, 1000);
+    assert_eq!(wallet_b.lamports, 1000);
+
+    tx = Transaction::new_with_payer(
         &[Instruction::new_with_borsh(
             program_id,
             &SwapInstruction::SwapAtoB { amount: 500 } ,
@@ -125,5 +132,6 @@ async fn test_swap() {
 
     assert_eq!(wallet_a.lamports, 1305);
     assert_eq!(wallet_b.lamports, 767);
+
 }
 
